72891a14380d2c7ab1e6797ec674a5d15a791b7f
Ant_Rob <rapiza06@gmail.com>
2021-08-05 23:42:31 -0500

    Change in the Stop

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <dimmable_light.h>
-
 //define Pin para dimmers
 #define syncPin 23
 #define dimmerPin 22
@@ -12,13 +11,18 @@
 #define PinPWM_rigth 18
 #define channelPinA 5
 //Señalizacion - paro de emergencia
-#define  PinbtnStop 4
+#define  PinbtnStop 17 //VERDE
+#define  PinbtnRun 16 //NARANJA
 #define  PinRun  2
 #define  PinStop 15
+//valves 
+#define PinOpen 32 
+#define PinClose 33
+#define PinOnOff 25
 
 //variables para establecer conexion
-const char* ssid = "PSmart 2019"; //"Depar";
-const char* password = "12345678";//"+-+/adgjl--++";
+const char* ssid = "Depar"; //"PSmart 2019"; //"Depar";
+const char* password = "+-+/adgjl--++";//"123456789";//"+-+/adgjl--++";
 const char* mqttServer = "192.168.0.101"; //192.168.0.100
 const int mqttPort = 1883;
 
@@ -26,33 +30,33 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 TaskHandle_t Task0;
 
-//Variables globales 
+//Variables globales******************************
 volatile int Speed_mt=0; //velocidad del motor 
 volatile int pot_res=0;  //valor para variar la pontencia de la resistencia de la tolva
 volatile int pot_res2=0; //valor para varia la potencia de la resistencia del reactor
 volatile int prc_vlv=0;  //valor para variar la apertura de la vlv proporcional
 volatile int bit_vlAlv=0; // bit para abrir o cerrar el vlv de alivio
 
-//Variables para los dimmers
+//Variables para los dimmers***********************
 DimmableLight rst_tolva(dimmerPin);
 DimmableLight rst_reactor(dimmerPin2);
 
-//variables motor y ajuste PWM
+//variables motor y ajuste PWM**********************
 const double  freq = 10000;
 const uint8_t Channel_left = 0;
 const uint8_t Channel_rigth = 5;
 const uint8_t resolution = 8;
 
-//lectura de velocidad del motor
+//lectura de velocidad del motor*********************
 volatile int count=0;
 portMUX_TYPE mux_enc = portMUX_INITIALIZER_UNLOCKED;
 double rpm=0,aux=0;
-//parametros filttro motor
+//parametros filttro motor****************************
 float Y_d = 0;  
 float alpha_d=0.1;
 float S_d=0.0;
 volatile int S_d_int=0;
-//variables para control PID
+//variables para control PID***************************
 //float err_ac=0;
 float err_an=0;
 //float derror=0;
@@ -63,20 +67,36 @@ float ki=0.85;
 float kd=0.04; 
 unsigned long current_time, prev_time;
 uint8_t dt_ms = 100;
-
-//vlv proporcional - vlv on-off
+//vlv proporcional*************************************
+int valor_vlvAnt = 0;
+int diff = 0; 
+int32_t tm_open = 5000000; //en microsegundos
+int32_t tm_close = 5000000; //en microsegundos
+volatile int interruptCounter;
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+bool inicia = false;
 
 //****FUNCIONES *********//
 void on_message(char* topic, byte* payload, unsigned int length);
 void reconnect();
 void setup_wifi();
 void IRAM_ATTR encoder();
+void IRAM_ATTR onTimer_vlv();
 int PID(int SP, int PV, uint8_t sp_ms);
 
 
 void loop2(void *parameter){
   for (;;){
-    if(digitalRead(PinbtnStop)){
+    if (interruptCounter > 0) {
+      portENTER_CRITICAL(&timerMux);
+      interruptCounter--;
+      portEXIT_CRITICAL(&timerMux);
+      digitalWrite(PinOpen,LOW);
+      digitalWrite(PinClose,LOW);
+    }
+    if(digitalRead(PinbtnRun)){
+      //Serial.println("\t\t\t\tEstoy en el nucleo 0");
       digitalWrite(PinRun,HIGH);
       digitalWrite(PinStop,LOW);
       current_time = millis();
@@ -89,7 +109,7 @@ void loop2(void *parameter){
         S_d_int=S_d;//velocidad en rpm filtrada
 
         //ledcWrite(Channel_rigth,Speed_mt); //sin control
-        ledcWrite(Channel_rigth,PID(Speed_mt,S_d_int,dt_ms)); //Con control
+        ledcWrite(Channel_rigth,Speed_mt); //Con control //PID(Speed_mt,S_d_int,dt_ms)
         ledcWrite(Channel_left,0);
         prev_time = current_time;
         portENTER_CRITICAL(&mux_enc);
@@ -98,13 +118,39 @@ void loop2(void *parameter){
       }
       rst_tolva.setBrightness(pot_res);
       rst_reactor.setBrightness(pot_res2);
-    }else {
+      if(bit_vlAlv==1){
+        digitalWrite(PinOnOff,HIGH);
+      }else{
+        digitalWrite(PinOnOff,LOW);
+      }
+      //Open of the valve
+      diff = prc_vlv - valor_vlvAnt;
+      if(diff>0 and diff<=100){
+        //Open valve
+        timerAlarmWrite(timer,tm_open*(diff/100), false);
+        timerAlarmEnable(timer);
+        digitalWrite(PinOpen,HIGH);
+      }
+      if(diff>=-100 and diff <0){
+        //Close valve
+        timerAlarmWrite(timer,tm_close*(-diff/100), false);
+        timerAlarmEnable(timer);
+        digitalWrite(PinClose,HIGH);
+      }
+      valor_vlvAnt  = prc_vlv;
+    }if(digitalRead(PinbtnStop)){
       Serial.println("PARO DE EMERGENCIA ACTIVADO");
       digitalWrite(PinRun,LOW);
       digitalWrite(PinStop,HIGH);
+      digitalWrite(PinOnOff,LOW);
+      digitalWrite(PinOpen,LOW);
+      digitalWrite(PinClose,LOW);
+      rst_tolva.setBrightness(0);
+      rst_reactor.setBrightness(0);
+      ledcWrite(Channel_rigth,0);
     }
+    vTaskDelay(10);
   }
-  vTaskDelay(10);
 }
 
 void setup() {
@@ -122,23 +168,46 @@ void setup() {
   ledcAttachPin(PinPWM_rigth,Channel_rigth);
 
   pinMode(PinbtnStop, INPUT_PULLDOWN);
+  pinMode(PinbtnRun,INPUT_PULLDOWN);
   pinMode(PinRun, OUTPUT);
   pinMode(PinStop,OUTPUT);
+  pinMode(PinOnOff,OUTPUT);
+  pinMode(PinClose,OUTPUT);
+  pinMode(PinOpen,OUTPUT);
+  
+  timer = timerBegin(3, 80, true);
+  timerAttachInterrupt(timer, &onTimer_vlv, true);
 
   pinMode(channelPinA, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(channelPinA),encoder, FALLING);
-  xTaskCreatePinnedToCore(loop2,"Actuators",1000,NULL,1,&Task0,0);
+  //xTaskCreatePinnedToCore(loop2,"Actuators",1000,NULL,1,&Task0,0);
   prev_time = millis();
 }
 
 void loop() {
   if(!client.connected()){
-     reconnect();
+    if(inicia == true){
+      Serial.println("Estoy dentro de loop 1");
+      vTaskDelete(Task0);
+      digitalWrite(PinRun,LOW);
+      digitalWrite(PinStop,HIGH);
+      digitalWrite(PinOnOff,LOW);
+      digitalWrite(PinOpen,LOW);
+      digitalWrite(PinClose,LOW);
+      rst_tolva.setBrightness(0);
+      rst_reactor.setBrightness(0);
+      ledcWrite(Channel_rigth,0);
+      inicia = false;
+    }
+    reconnect();
     }
   if(client.connected()){
     //Aqui siempre va ha estar entrando si es que estamos conectados al servidor 
     //Aqui se deben de enviar los mensajes hacia el servidor 
-    
+    if(inicia==false){
+      xTaskCreatePinnedToCore(loop2,"Actuators",10000,NULL,1,&Task0,0);
+      inicia = true;
+    }
     }
   client.loop(); 
 }
@@ -147,6 +216,13 @@ void IRAM_ATTR encoder(){
   portENTER_CRITICAL_ISR(&mux_enc);
   count++;
   portEXIT_CRITICAL_ISR(&mux_enc);
+}
+
+void IRAM_ATTR onTimer_vlv() {
+  portENTER_CRITICAL_ISR(&timerMux);
+  interruptCounter++;
+  portEXIT_CRITICAL_ISR(&timerMux);
+ 
 }
 
 int PID(int SP, int PV, uint8_t sp_ms){
@@ -180,13 +256,13 @@ void setup_wifi() {
 
 void reconnect(){
   while (!client.connected()){
-    Serial.println("Connecting to MQTT...");
+    Serial.println("Connecting to MQTT.................................................................");
     //Crea un cliente ID
     String clientId = "Micro_ESP32";
     clientId += String(random(0xffff),HEX);
     //Intentamos conectar
     if (client.connect(clientId.c_str())) {
-      Serial.println("connected");
+      Serial.println("*******CONNECTED*******");
       //Sucribirse a los temas 
       client.subscribe("planta/tolva/val_Pro");
       client.subscribe("planta/tolva/res_elec");
@@ -194,8 +270,8 @@ void reconnect(){
       client.subscribe("planta/reactor/motor");
       client.subscribe("planta/reactor/val_alv");
     }else{
-      Serial.print("failed with state");
-      Serial.print(client.state());
+      Serial.print("failed with state -->");
+      Serial.println(client.state());
       delay(2000);
     }
   }
